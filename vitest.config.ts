f727969841c2ed37d0881b84c905925d522a8d54
Joydip Roy <joydipand@gmail.com>
2023-12-07 11:57:49 +0530

    chore: removed vite deps, plugins and bring patches for the vscode issue in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import { defineConfig } from 'vitest/config'
 
-export default mergeConfig(viteConfig, defineConfig({
+export default defineConfig({
   test: {
     exclude: ['**/src/**', '**/node_modules/**'],
   },
-}))
+})
